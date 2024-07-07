56f3c5f2fcf1a4f135cfd1212b19ec716a63e700
RAJAT SONAR <150504303+rajatsonar22@users.noreply.github.com>
2024-07-07 08:53:39 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,7 @@
+public class forloop {
+    public static void main(String[] args) {
+        for(int counter=0;counter <45;counter=counter+1){
+            System.out.println("hlo world ");
+        }
+    }
+}
